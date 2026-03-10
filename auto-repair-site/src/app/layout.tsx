62ea2a5c97f99f412d0b9543885b0ec9ac171676
Trevor Riggs <triggsmt67@gmail.com>
2026-03-09 21:51:49 -0600

    perf: refactored GoogleTagManager to use lazyOnload, preventing 5s main thread lock and fixing LCP render delay

--- auto-repair-site/src/app/layout.tsx
+++ auto-repair-site/src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/components/widgets/header";
 import { Footer } from "@/components/widgets/footer";
 import { generateLocalBusinessSchema, generateWebSiteSchema } from "@/lib/seo";
 import { siteConfig } from "@/lib/siteConfig";
-import { GoogleTagManager } from '@next/third-parties/google';
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -52,7 +52,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head>
-        <GoogleTagManager gtmId="GTM-WBTQJ8DS" />
         {/* Preconnect hints to reduce critical path latency */}
         <link rel="preconnect" href="https://www.googletagmanager.com" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
@@ -66,6 +65,29 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${playfair.variable} font-sans antialiased min-h-screen flex flex-col`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WBTQJ8DS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        <Script
+          id="gtm-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', 'GTM-WBTQJ8DS');
+            `,
+          }}
+        />
 
         <Header />
         <main className="flex-1">{children}</main>
